992fbf71c9e1c601627c2814522c3256e4e9695b
Ribesg <Ribesg@Dragon>
2012-12-20 20:10:25 +0100

    Link to Core Interfaces

--- src/com/github/ribesg/npunisher/NPunisher.java
+++ src/com/github/ribesg/npunisher/NPunisher.java
@@ -2,6 +2,27 @@ package com.github.ribesg.npunisher;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class NPunisher extends JavaPlugin {
+import com.github.ribesg.ncore.NCore;
+import com.github.ribesg.ncore.nodes.punisher.PunisherNode;
+
+public class NPunisher extends JavaPlugin implements PunisherNode {
+
+	@Override
+	public void afterEnable() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setCore(NCore core) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public NCore getCore() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
