0fa720dc15a42033ac0afaf8246d710cc4a4e2fa
Ribesg <Ribesg@Dragon>
2012-12-20 22:44:21 +0100

    -

--- src/com/github/ribesg/npunisher/NPunisher.java
+++ src/com/github/ribesg/npunisher/NPunisher.java
@@ -1,28 +1,58 @@
 package com.github.ribesg.npunisher;
 
+import lombok.Getter;
+
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.github.ribesg.ncore.NCore;
+import com.github.ribesg.ncore.nodes.cuboid.CuboidNode;
 import com.github.ribesg.ncore.nodes.punisher.PunisherNode;
 
 public class NPunisher extends JavaPlugin implements PunisherNode {
 
-	@Override
-	public void afterEnable() {
-		// TODO Auto-generated method stub
+    // Core plugin
+    @Getter NCore      core;
 
-	}
+    // Useful Nodes
+    @Getter CuboidNode cuboidNode;
 
-	@Override
-	public void setCore(NCore core) {
-		// TODO Auto-generated method stub
+    // Set to true by afterEnable() call
+    // Prevent multiple calls to afterEnable
+    private boolean    loadingComplete = false;
 
-	}
+    @Override
+    public void onEnable() {
+        if (!Bukkit.getPluginManager().isPluginEnabled("NCore")) {
+            // TODO
+        } else {
+            // TODO
 
-	@Override
-	public NCore getCore() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        }
+    }
+
+    public void afterEnable() {
+        if (!this.loadingComplete) {
+            this.loadingComplete = true;
+            Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+
+                @Override
+                public void run() {
+                    // Interact with other Nodes here
+
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    public void setCore(final NCore core) {
+        this.core = core;
+        core.setPunisherNode(this);
+    }
 
 }
